d499421dcb0b9dac394afc6c2298946371c6ed54
MangoSwirl <68089814+MangoSwirl@users.noreply.github.com>
2023-11-19 00:28:46 -0800

    Delete dist directory

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import RouteLoader from "./RouteLoader.js";
-const app = express();
-const port = process.env.PORT || 3000;
-const routes = await RouteLoader('dist/routes/**/*.ts');
-app.listen(port, () => console.log('Server is running'));
--- dist/routes/manager/example.js
+++
@@ -1,6 +0,0 @@
-export default (router) => {
-    router.get('/manager/example', async (req, res) => {
-        res.status(200).send('All good my dude');
-    });
-    return router;
-};
