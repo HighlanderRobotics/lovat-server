b305b71f1cf0798959d1aab9ee8ed9eb7ef4d235
MangoSwirl <collin.camerono@gmail.com>
2023-11-24 15:49:19 -0800

    Format file

--- src/handler/manager/addWebsite.ts
+++ src/handler/manager/addWebsite.ts
@@ -9,33 +9,31 @@ import { sendSlackVerification } from "./sendSlackVerification";
 export const addWebsite = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
     try {
         const user = await getUser(req, res)
-        if(user === null)
-        {
+        if (user === null) {
             return
         }
         const WebsiteSchema = z.object({
-            website : z.string()
+            website: z.string()
         })
-        const currWebsite = {website : req.body.website}
+        const currWebsite = { website: req.body.website }
         const possibleTypeErrorShift = WebsiteSchema.safeParse(currWebsite)
         if (!possibleTypeErrorShift.success) {
             res.status(400).send(possibleTypeErrorShift)
             return
-        }        const row = await prismaClient.registeredTeam.update({
-            where : {
-                number : user.teamNumber
+        } const row = await prismaClient.registeredTeam.update({
+            where: {
+                number: user.teamNumber
             },
-            data : currWebsite
+            data: currWebsite
         })
 
-        await sendSlackVerification(res, row.number, row.email, req.body.website)        
+        await sendSlackVerification(res, row.number, row.email, req.body.website)
     }
-    catch(error)
-    {
+    catch (error) {
         console.error(error)
         res.status(400).send(error)
     }
-    
+
 };
 
 
