404aae74e6e87fc65f6346f047e0f8448e1dd506
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-01-20 15:16:51 -0800

    seed added

---
+++ seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  const featureToggleSlackVerification = await prisma.featureToggle.create({
+    data :
+    {
+        feature : "fullRegistration",
+        enabled : false
+    }
+  }) 
+
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
