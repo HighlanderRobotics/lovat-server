70ceb3dbfc9d681b9dc25afde744891f1aaefeee
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-01-21 21:48:10 -0800

    getTeamSource updated

--- src/handler/manager/getTeamSource.ts
+++ src/handler/manager/getTeamSource.ts
@@ -6,7 +6,21 @@ import { AuthenticatedRequest } from "../../lib/middleware/requireAuth";
 
 export const getTeamSource = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
     try {
-        res.status(200).send(req.user.teamSource)
+        if(req.user.teamSource.length === 1 && req.user.teamSource[0] ===req.user.teamNumber)
+        {
+            res.status(200).send("THIS_TEAM")
+            return
+        }
+        const team = await prismaClient.team.findMany()
+        if(req.user.teamSource.length === team.length)
+        {
+            res.status(200).send("ALL_TEAMS")
+            return
+        }
+        else
+        {
+            res.status(200).send(req.user.teamSource)
+        }
     }
     catch (error) {
         console.error(error);
