8b8cae94d53b915000cfac150467fecf22ee8aff
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-02-18 16:06:35 -0800

    logging for Sophie

--- src/handler/manager/getMatches.ts
+++ src/handler/manager/getMatches.ts
@@ -212,12 +212,19 @@ export const getMatches = async (req: AuthenticatedRequest, res: Response): Prom
                 res.status(400).send(`Matches not added correctly, does not have 6 teams for match ${element.matchNumber} of type ${element.matchType}`)
                 return
             }
+            if(currMatch[0].matchNumber === 6)
+            {
+                console.log(currMatch)
+            }
             currMatch = currMatch.sort((a, b) => {
                 const lastDigitA = parseInt(a[a.key.length - 1], 10);
                 const lastDigitB = parseInt(b[b.key.length - 1], 10);
-                 return lastDigitA - lastDigitB;
+                return lastDigitA - lastDigitB;
               });
-            let currData = {
+              if(currMatch[0].matchNumber === 6)
+              {
+                  console.log(currMatch)
+              }            let currData = {
                 tournamentKey: params.data.tournamentKey, matchNumber: element.matchNumber, matchType: ReverseMatchTypeMap[element.matchType], scouted: element.scouted,
                 team1: { number: currMatch[0].teamNumber, alliance: "red", scouters: [] },
                 team2: { number: currMatch[1].teamNumber, alliance: "red", scouters: [] },
