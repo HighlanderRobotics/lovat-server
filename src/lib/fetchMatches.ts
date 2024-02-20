9b22e7d2db6f712fe1371d356509be7a52801435
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-02-19 20:02:09 -0800

    fetching tournaments within the week

--- src/lib/fetchMatches.ts
+++ src/lib/fetchMatches.ts
@@ -7,16 +7,28 @@ import { date } from "zod";
 export default async function fetchMatches() {
     // upsert current tournaments in the matches table
     //new tournaments are added to the matches table in getMatches (if it doesn't already exist)
+
+    let startOfWeek = new Date();
+    startOfWeek.setDate(startOfWeek.getDate() - 3);
+
+    let endOfWeek = new Date();
+    endOfWeek.setDate(endOfWeek.getDate() + 3);
+
+    const oneWeekAgo = new Date();
     const distinctTournamentKeys = await prisma.teamMatchData.groupBy({
         by: ['tournamentKey'],
-        //DO WITHIN THE
-        // where :
-        // {
-        //     tournament :
-        //     {
-        //         date : Date()
-        //     }
-        // }
+        //does within the week
+        where :
+        {
+            tournament :
+            {
+                date :
+                {
+                    gte : startOfWeek.toDateString(),
+                    lte : endOfWeek.toDateString()
+                }
+            }
+        }
     });
     for(const tournamentKeyRow of distinctTournamentKeys)
     {
