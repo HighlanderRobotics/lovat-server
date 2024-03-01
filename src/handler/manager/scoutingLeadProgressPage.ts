22802fbe330af0702062f54aae0c1097c6e582c1
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-02-29 17:39:13 -0800

    misMatched and flags on picklists

--- src/handler/manager/scoutingLeadProgressPage.ts
+++ src/handler/manager/scoutingLeadProgressPage.ts
@@ -6,38 +6,83 @@ import { AuthenticatedRequest } from "../../lib/middleware/requireAuth";
 
 export const scoutingLeadProgressPage = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
     try {
-        if(req.user.teamNumber === null)
-        {
+
+        const params = z.object({
+            tournamentKey: z.string().nullable()
+        }).safeParse({
+            tournamentKey: req.query.tournamentKey || null
+        })
+        if (!params.success) {
+            res.status(400).send(params);
+            return;
+        };
+        if (req.user.teamNumber === null) {
             res.status(400).send("Not affilated with a team")
             return
         }
-        if(req.user.role !== "SCOUTING_LEAD")
-        {
+        if (req.user.role !== "SCOUTING_LEAD") {
             res.status(400).send("Not a scouting lead")
             return
         }
-        //ADD MORE INFO LATER ONCE WE DECIDE WHAT WE WANT TO SHOW
         const scouters = await prismaClient.scouter.findMany({
-            where :
+            where:
             {
-                sourceTeamNumber : req.user.teamNumber
+                sourceTeamNumber: req.user.teamNumber
             },
-            include :
+            include:
             {
-               scoutReports : true
+                scoutReports: true
             }
         })
-        const formattedScouters = scouters.map(scouter => ({
-            scouterUuid: scouter.uuid,
-            scouterName: scouter.name,
-            matchesScouted: scouter.scoutReports.length
-          }));
-        res.status(200).send(formattedScouters)
+        if (params.data.tournamentKey) {
+            const totalMatchesAtTournaent = await prismaClient.teamMatchData.groupBy({
+                by: ["matchType", "matchNumber"],
+                where:
+                {
+                    tournamentKey: params.data.tournamentKey,
+                    scoutReports:
+                    {
+                        some:
+                            {}
+                    }
+                }
+            })
+            console.log(totalMatchesAtTournaent)
+            const totalMatchesScoutedAtTournament = totalMatchesAtTournaent.length
+            let result = []
+            for(const scouter of scouters)
+            {
+                const matchesScoutedAtTournament = await prismaClient.scoutReport.groupBy({
+                    by : ["teamMatchKey"],
+                    where :
+                    {
+                        scouterUuid : scouter.uuid,
+                        teamMatchData :
+                        {
+                            tournamentKey : params.data.tournamentKey
+                        }
+                    }
+                })
+                const currData = {scouterUuid : scouter.uuid, scouterName : scouter.name, matchesScouted : matchesScoutedAtTournament.length, missedMatches : totalMatchesScoutedAtTournament-matchesScoutedAtTournament.length}
+                result.push(currData)
+            }
+            
+            res.status(200).send(result)
+
+        }
+        else {
+            const formattedScouters = scouters.map(scouter => ({
+                scouterUuid: scouter.uuid,
+                scouterName: scouter.name,
+                matchesScouted: scouter.scoutReports.length,
+                missedMatches : 0
+            }));
+            res.status(200).send(formattedScouters)
+        }
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error)
         res.status(500).send(error)
     }
-    
+
 };
