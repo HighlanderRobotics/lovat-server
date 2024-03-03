512303f149b5c3b0bc8f9cb47a5b0e24b868b884
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-03-02 16:35:55 -0800

    missed matches correct + add scouter dashboard

--- src/handler/manager/scoutingLeadProgressPage.ts
+++ src/handler/manager/scoutingLeadProgressPage.ts
@@ -24,18 +24,9 @@ export const scoutingLeadProgressPage = async (req: AuthenticatedRequest, res: R
             res.status(400).send("Not a scouting lead")
             return
         }
-        const scouters = await prismaClient.scouter.findMany({
-            where:
-            {
-                sourceTeamNumber: req.user.teamNumber
-            },
-            include:
-            {
-                scoutReports: true
-            }
-        })
+       
         if (params.data.tournamentKey) {
-            const totalMatchesAtTournaent = await prismaClient.teamMatchData.groupBy({
+            const matchesAtTournamentRows = await prismaClient.teamMatchData.groupBy({
                 by: ["matchType", "matchNumber"],
                 where:
                 {
@@ -47,11 +38,103 @@ export const scoutingLeadProgressPage = async (req: AuthenticatedRequest, res: R
                     }
                 }
             })
-            console.log(totalMatchesAtTournaent)
-            const totalMatchesScoutedAtTournament = totalMatchesAtTournaent.length
+            const totalTournamentMatches = matchesAtTournamentRows.length
+            const scouters = await prismaClient.scouter.findMany({
+                where:
+                {
+                    sourceTeamNumber: req.user.teamNumber
+                },
+                include:
+                {
+                    scoutReports: true,
+                    Team1Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    },
+                    Team2Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    },
+                    Team3Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    },
+                    Team4Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    },
+                    Team5Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    },
+                    Team6Shifts : {
+                        where :
+                        {
+                            tournamentKey : params.data.tournamentKey,
+                            startMatchOrdinalNumber :
+                            {
+                                lte : totalTournamentMatches
+                            }
+                        }
+                    }
+                }
+            })
+           
             let result = []
             for(const scouter of scouters)
             {
+                const allShifts = [
+                    ...scouter.Team1Shifts,
+                    ...scouter.Team2Shifts,
+                    ...scouter.Team3Shifts,
+                    ...scouter.Team4Shifts,
+                    ...scouter.Team5Shifts,
+                    ...scouter.Team6Shifts
+                ];
+            
+                // iterate over all shifts and sum total assigned matches
+                let totalAssignedScouterMatches = 0
+                allShifts.forEach(shift => {
+                    const matchesForShift = (shift.endMatchOrdinalNumber - shift.startMatchOrdinalNumber);
+                    if(shift.endMatchOrdinalNumber > totalTournamentMatches)
+                    {
+                        totalAssignedScouterMatches += totalTournamentMatches - shift.startMatchOrdinalNumber
+                    }
+                    else
+                    {
+                        totalAssignedScouterMatches += matchesForShift;
+                    }
+                });
                 const matchesScoutedAtTournament = await prismaClient.scoutReport.groupBy({
                     by : ["teamMatchKey"],
                     where :
@@ -63,7 +146,7 @@ export const scoutingLeadProgressPage = async (req: AuthenticatedRequest, res: R
                         }
                     }
                 })
-                const currData = {scouterUuid : scouter.uuid, scouterName : scouter.name, matchesScouted : matchesScoutedAtTournament.length, missedMatches : totalMatchesScoutedAtTournament-matchesScoutedAtTournament.length}
+                const currData = {scouterUuid : scouter.uuid, scouterName : scouter.name, matchesScouted : matchesScoutedAtTournament.length, missedMatches : Math.max(0, totalAssignedScouterMatches-matchesScoutedAtTournament.length)}
                 result.push(currData)
             }
             
@@ -71,6 +154,16 @@ export const scoutingLeadProgressPage = async (req: AuthenticatedRequest, res: R
 
         }
         else {
+            const scouters = await prismaClient.scouter.findMany({
+                where :
+                {
+                    sourceTeamNumber : req.user.teamNumber
+                },
+                include :
+                {
+                    scoutReports : true
+                }
+            })
             const formattedScouters = scouters.map(scouter => ({
                 scouterUuid: scouter.uuid,
                 scouterName: scouter.name,
