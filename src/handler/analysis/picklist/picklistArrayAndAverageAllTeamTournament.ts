a3076c27ababfdf82e8b3736eed5ff5c3411c283
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-03-23 14:26:26 -0700

    timeout

--- src/handler/analysis/picklist/picklistArrayAndAverageAllTeamTournament.ts
+++ src/handler/analysis/picklist/picklistArrayAndAverageAllTeamTournament.ts
@@ -17,7 +17,7 @@ export const picklistArrayAndAverageAllTeamTournament = async (user: User, metri
         for (const team of teams) {
             const currAvg = ( arrayAndAverageTeamFast(user, metric, team))
             timeLineArray.push(currAvg)
-            await wait(2)
+            await wait(10)
         };
         //change to null possibly
         let average = 0
