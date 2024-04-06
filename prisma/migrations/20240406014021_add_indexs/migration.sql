edd5699bf4126cbaec928e182f052f5a5156605f
CassieColby <70112547+CassieColby@users.noreply.github.com>
2024-04-05 18:40:51 -0700

    indexing

---
+++ prisma/migrations/20240406014021_add_indexs/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "Event_scoutReportUuid_idx" ON "Event"("scoutReportUuid");
+
+-- CreateIndex
+CREATE INDEX "Scouter_sourceTeamNumber_idx" ON "Scouter"("sourceTeamNumber");
