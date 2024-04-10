88794d2c7d1ed746838cf64fcd0f64cc2a02f563
Sid T <thomas.siddharth@gmail.com>
2024-04-10 14:48:53 -0700

    Add CSV export (#6)
    * version updates
    
    * Add export-to-csv package
    
    * Error check metric keys at compile time
    
    this is pretty scuffed i think but it works
    
    * use csv-stringify instead of export-to-csv
    
    * Boilerplate code for getCSV, still needs aggregation math (mostly looping)
    
    * Implement all aggregation except for points + stage
    
    * Add failsafe for robot roles
    
    * Implement aggregation for stage and match points
    
    * Add number of scouted matches to export
    
    * Remember to divide sums to get mean
    
    * Add csvplain route to express
    
    * Commenting run
    
    * Limit CSV to data from given tournament
    
    * Set up all data points for csv
    
    * Implement 'event' data points for csv
    
    * remove averages for high notes
    
    didnt make sense because teams may not attempt it
    
    * Round csv output data to nearest hundredth
    
    * smacks head
    
    * Add edge case for no matches immobile
    
    * Stage interactions export as percentages
    
    * whoops, fixed percentage for stage harmony
    
    * Add percentage of matches without stage interaction
    
    * Fix issues with main robot role and matches immobile
    
    * Add stage points to teleop
    
    * empty data object so js compiles
    
    ---------
    
    Co-authored-by: CassieColby <70112547+CassieColby@users.noreply.github.com>

--- src/handler/analysis/coreAnalysis/nonEventMetric.ts
+++ src/handler/analysis/coreAnalysis/nonEventMetric.ts
@@ -4,15 +4,18 @@ import z from 'zod'
 import { AuthenticatedRequest } from "../../../lib/middleware/requireAuth";
 import { singleMatchEventsAverage } from "./singleMatchEventsAverage";
 import { arrayAndAverageTeam } from "./arrayAndAverageTeam";
-import { User } from "@prisma/client";
+import { User, ScoutReport } from "@prisma/client";
 
 
 export const nonEventMetric = async (user: User, team : number, metric: string): Promise<Object> => {
     try {
         const params = z.object({
-            //UPDATE WITH COLUMNS IN THE SCHEMA EACH YEAR
+            // UPDATE WITH COLUMNS IN THE SCHEMA EACH YEAR
             column: z.enum([
-                "robotRole", "pickUp", "highNote", "stage"
+                checkPropertyKey<ScoutReport>("robotRole"),
+                checkPropertyKey<ScoutReport>("pickUp"),
+                checkPropertyKey<ScoutReport>("highNote"),
+                checkPropertyKey<ScoutReport>("stage")
             ]),
             team: z.number()
         }).safeParse({
@@ -50,7 +53,7 @@ export const nonEventMetric = async (user: User, team : number, metric: string):
         })
         const totalCount = countArray.reduce((acc, group) => acc + group._count.scouterUuid, 0);
         const transformedData = countArray.reduce((acc, group) => {
-            const columnValue = group[params.data.column].toLowerCase();
+            const columnValue = group[params.data.column as string].toLowerCase();
             const percentage = group._count.scouterUuid / totalCount;
             acc[columnValue] = percentage;
             return acc;
@@ -61,5 +64,9 @@ export const nonEventMetric = async (user: User, team : number, metric: string):
         console.error(error)
         throw (error)
     }
-
 };
+
+// Throws compiler error if keys do not match properties
+function checkPropertyKey<T>(key: keyof T) {
+    return key;
+}
---
+++ src/handler/manager/getCSV.ts
@@ -0,0 +1,338 @@
+import { Response } from "express";
+import prismaClient from "../../prismaClient"
+import { AuthenticatedRequest } from "../../lib/middleware/requireAuth";
+import { stringify } from 'csv-stringify/sync';
+import { UserRole, RobotRole, StageResult, HighNoteResult, PickUp, EventAction, Position } from "@prisma/client";
+import { autoEnd } from "../analysis/analysisConstants";
+import { z } from "zod";
+
+type CSVData = {
+    teamNumber: number
+    mainRole: string
+    secondaryRole: string
+    groundPickup: boolean
+    chutePickup: boolean
+    avgTeleopPoints: number
+    avgAutoPoints: number
+    avgDriverAbility: number
+    avgPickups: number
+    avgFeeds: number
+    avgDrops: number
+    avgScores: number
+    avgAmpScores: number
+    avgSpeakerScores: number
+    avgTrapScores: number
+    avgDefense: number
+    // avgOffensePoints: number
+    percStagePark: number
+    percStageClimb: number
+    percStageClimbHarmony: number
+    percStageNone: number
+    highNoteFails: number
+    highNoteSuccesses: number
+    matchesImmobile: number
+    numMatches: number
+    numReports: number
+}
+
+// Simplified scouting report containing only the properties required for aggregation
+type PointsReport = {
+    robotRole: RobotRole
+    stage: StageResult
+    highNote: HighNoteResult
+    pickUp: PickUp
+    driverAbility: number
+    events: {
+        time: number
+        action: EventAction
+        position: Position
+        points: number
+    }[]
+    // This property represents the weighting of this report in the final aggregation [0..1]
+    weight: number
+}
+
+export const getCSV = async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+    try {
+        if (req.user.role !== UserRole.SCOUTING_LEAD) {
+            res.status(403).send("Not authorized to download scouting data");
+            return;
+        }
+
+        // Data will only be sourced from a tournament as sent with the request
+        const params = z.object({
+            tournamentKey: z.string()
+        }).safeParse({
+            tournamentKey: req.query.tournamentKey
+        });
+
+        if (!params.success) {
+            res.status(400).send({ "error": params, "displayError": "Invalid tournament selected." });
+            return;
+        }
+
+        // TeamMatchData instances have unique combinations of team, match, and tournament keys
+        // These instances will be sorted by team number and then looped through and aggregated
+        const datapoints = await prismaClient.teamMatchData.findMany({
+            where: {
+                tournamentKey: params.data.tournamentKey
+            },
+            select: {
+                teamNumber: true,
+                scoutReports: {
+                    where: {
+                        scouter: {
+                            sourceTeamNumber: {
+                                in: req.user.teamSource
+                            }
+                        }
+                    },
+                    select: {
+                        robotRole: true,
+                        stage: true,
+                        highNote: true,
+                        pickUp: true,
+                        driverAbility: true,
+                        events: {
+                            select: {
+                                time: true,
+                                action: true,
+                                position: true,
+                                points: true
+                            }
+                        }
+                    }
+                }
+            },
+            orderBy: {
+                teamNumber: "desc"
+            }
+        });
+
+        if (datapoints.length === 0) {
+            res.status(400).send("Not enough scouting data from provided sources");
+            return;
+        }
+
+        // Group reports by team number in a sparse array
+        const groupedByTeam = datapoints.reduce<{reports: PointsReport[], numMatches: number}[]>((acc, cur) => {
+            acc[cur.teamNumber] = acc[cur.teamNumber] || {reports: [], numMatches: 0};
+
+            // Increment number of matches for team
+            acc[cur.teamNumber].numMatches++;
+
+            // Push reports for team from match
+            cur.scoutReports.forEach(element => {
+                acc[cur.teamNumber].reports.push(Object.assign({weight: 1/cur.scoutReports.length}, element));
+            });
+
+            return acc;
+        }, []);
+
+        // Aggregate point values
+        const aggregatedData: CSVData[] = [];
+        groupedByTeam.forEach((group, teamNum) => {
+            aggregatedData.push(aggregatePointsReports(teamNum, group.numMatches, group.reports));
+        });
+
+        // Create and send the csv string through express
+        const csvString = stringify(aggregatedData, {
+            header: true,
+            // Creates csv headers from data properties
+            columns: Object.keys(aggregatedData[0]),
+            // Required for excel viewing
+            bom: true
+        });
+        res.attachment("lovatDownload.csv");
+        res.header('Content-Type', 'text/csv');
+        res.send(csvString);
+        return;
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+function aggregatePointsReports(teamNum: number, numMatches: number, reports: PointsReport[]): CSVData {
+    let data: CSVData = {
+        teamNumber: 0,
+        mainRole: "",
+        secondaryRole: "",
+        groundPickup: false,
+        chutePickup: false,
+        avgTeleopPoints: 0,
+        avgAutoPoints: 0,
+        avgDriverAbility: 0,
+        avgPickups: 0,
+        avgFeeds: 0,
+        avgDrops: 0,
+        avgScores: 0,
+        avgAmpScores: 0,
+        avgSpeakerScores: 0,
+        avgTrapScores: 0,
+        avgDefense: 0,
+        percStagePark: 0,
+        percStageClimb: 0,
+        percStageClimbHarmony: 0,
+        percStageNone : 0,
+        highNoteFails: 0,
+        highNoteSuccesses: 0,
+        matchesImmobile: 0,
+        numMatches: 0,
+        numReports: 0
+    };    data.teamNumber = teamNum;
+    data.numMatches = numMatches;
+    data.numReports = reports.length;
+
+    const roles: Record<RobotRole, number> = {
+        OFFENSE: 0,
+        DEFENSE: 0,
+        FEEDER: 0,
+        IMMOBILE: 0
+    };
+    // Main iteration for most aggregation summing
+    reports.forEach(report => {
+        // Sum driver ability and robot role
+        data.avgDriverAbility += report.driverAbility * report.weight;
+        roles[report.robotRole] += report.weight;
+
+        // Implement a safety for this? One incorrect report could mess up the data
+        // Set if chute/groud pickup has been observed
+        data.chutePickup ||= report.pickUp !== PickUp.GROUND;
+        data.groundPickup ||= report.pickUp !== PickUp.CHUTE;
+
+        // Sum stage results
+        switch (report.stage) {
+            case StageResult.PARK:
+                data.percStagePark += report.weight;
+                break;
+            case StageResult.ONSTAGE:
+                data.percStageClimb += report.weight;
+                break;
+            case StageResult.ONSTAGE_HARMONY:
+                data.percStageClimbHarmony += report.weight;
+                break;
+            case StageResult.NOTHING:
+                data.percStageNone += report.weight;
+                break;
+        }
+
+        // Sum high note successes/failures
+        if (report.highNote === HighNoteResult.SUCCESSFUL) {
+            data.highNoteSuccesses += report.weight;
+        } else if (report.highNote === HighNoteResult.FAILED) {
+            data.highNoteFails += report.weight;
+        }
+
+        // Sum match points and actions
+        report.events.forEach(event => {
+            if (event.time < autoEnd) {
+                data.avgAutoPoints += event.points * report.weight;
+            } else {
+                data.avgTeleopPoints += event.points * report.weight;
+            }
+
+            switch (event.action) {
+                case EventAction.DEFENSE:
+                    data.avgDefense += report.weight;
+                    break;
+                case EventAction.DROP_RING:
+                    data.avgDrops += report.weight;
+                    break;
+                case EventAction.FEED_RING:
+                    data.avgFeeds += report.weight;
+                    break;
+                case EventAction.PICK_UP:
+                    data.avgPickups += report.weight;
+                    break;
+                case EventAction.SCORE:
+                    data.avgScores += report.weight;
+                    switch (event.position) {
+                        case Position.AMP:
+                            data.avgAmpScores += report.weight;
+                            break;
+                        case Position.SPEAKER:
+                            data.avgSpeakerScores += report.weight;
+                            break;
+                        case Position.TRAP:
+                            data.avgTrapScores += report.weight;
+                            break;
+                    }
+                    break;
+            }
+
+            // FIX: This will only work if all reports for a match mark robot role as offense
+            // if (report.robotRole === RobotRole.OFFENSE) {
+            //     data.avgOffensePoints += event.points * report.weight;
+            // }
+        });
+    });
+
+    data.matchesImmobile = roles.IMMOBILE || 0;
+    // Remove IMMOBILE state from main roles
+    delete roles.IMMOBILE;
+
+    // Main method to find highest reported roles
+    const highestOccurences = Object.entries(roles).reduce((highestOccurences, role) => {
+        // Using >= gives precedence to lower-frequency roles such as Feeder
+        if (role[1] >= highestOccurences[1]) {
+            if (role[1] >= highestOccurences[0]) {
+                // Push main role to secondary
+                highestOccurences[1] = highestOccurences[0];
+                data.secondaryRole = data.mainRole;
+
+                // Push new role to main
+                highestOccurences[0] = role[1];
+                data.mainRole = role[0];
+            } else {
+                // Push new role to secondary
+                highestOccurences[1] = role[1];
+                data.secondaryRole = role[0];
+            }
+        }
+
+        return highestOccurences;
+    }, [0, 0]);
+
+    // Failsafe if robot lacks reports for enough roles
+    if (highestOccurences[1] === 0) {
+        data.secondaryRole = "NONE"
+        if (highestOccurences[0] === 0) {
+            data.mainRole = RobotRole.IMMOBILE;
+        }
+    }
+
+    // Add stage points to teleop
+    data.avgTeleopPoints += data.percStagePark + data.percStageClimb * 3 + data.percStageClimbHarmony * 5;
+
+    // Divide relevent sums by number of matches to get mean
+    data.avgTeleopPoints = roundToHundredth(data.avgTeleopPoints / numMatches);
+    data.avgAutoPoints = roundToHundredth(data.avgAutoPoints / numMatches);
+    data.avgDriverAbility = roundToHundredth(data.avgDriverAbility / numMatches);
+    data.avgPickups = roundToHundredth(data.avgPickups / numMatches);
+    data.avgFeeds = roundToHundredth(data.avgFeeds / numMatches);
+    data.avgDrops = roundToHundredth(data.avgDrops / numMatches);
+    data.avgScores = roundToHundredth(data.avgScores / numMatches);
+    data.avgAmpScores = roundToHundredth(data.avgAmpScores / numMatches);
+    data.avgSpeakerScores = roundToHundredth(data.avgSpeakerScores / numMatches);
+    data.avgTrapScores = roundToHundredth(data.avgTrapScores / numMatches);
+    data.avgDefense = roundToHundredth(data.avgDefense / numMatches);
+    // data.avgOffensePoints = roundToHundredth(data.avgOffensePoints / numMatches);
+
+    data.percStagePark = Math.round(data.percStagePark / numMatches * 1000) / 10;
+    data.percStageClimb = Math.round(data.percStageClimb / numMatches * 1000) / 10;
+    data.percStageClimbHarmony = Math.round(data.percStageClimbHarmony / numMatches * 1000) / 10;
+    data.percStageNone = Math.round(data.percStageNone / numMatches * 1000) / 10;
+
+    // Trim remaining datapoints
+    data.highNoteFails = roundToHundredth(data.highNoteFails);
+    data.highNoteSuccesses = roundToHundredth(data.highNoteSuccesses);
+    data.matchesImmobile = roundToHundredth(data.matchesImmobile);
+
+    return data;
+}
+
+function roundToHundredth(a: number): number {
+    return Math.round(a * 100) / 100;
+}
