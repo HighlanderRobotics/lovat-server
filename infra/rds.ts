50298a9d712357107ef62688d6f740200d2d7849
MangoSwirl <collin.camerono@gmail.com>
2024-10-17 15:46:56 -0700

    Increase migration timeout

--- infra/rds.ts
+++ infra/rds.ts
@@ -8,6 +8,7 @@ const migrationFunction = new sst.aws.Function("DeploymentMigrate", {
   vpc,
   link: [rds],
   handler: "packages/api/src/migrateHandler.handler",
+  timeout: "60 seconds",
   environment: {
     HOME: "/tmp",
   },
@@ -32,77 +33,129 @@ if (!$dev) {
 }
 
 // MARK: TBA Data
-const fetchTournamentsCron = new sst.aws.Cron("FetchTournamentsCron", {
-  job: {
-    vpc,
-    handler: "packages/api/src/lib/getTBAData.fetchTournamentsHandler",
-    link: [rds, tbaKey],
-    timeout: "60 seconds",
-    copyFiles: [
-      {
-        from: "node_modules/.prisma/client",
-        to: "packages/.prisma/client",
-      },
-    ],
-  },
+const fetchTournamentsCron = new sst.aws.Cron(
+  "FetchTournamentsCron",
+  {
+    job: {
+      vpc,
+      handler: "packages/api/src/lib/getTBAData.fetchTournamentsHandler",
+      link: [rds, tbaKey],
+      timeout: "60 seconds",
+      copyFiles: [
+        {
+          from: "node_modules/.prisma/client",
+          to: "packages/.prisma/client",
+        },
+      ],
+    },
 
-  schedule: "rate(10 days)",
-});
-
-const fetchTeamsCron = new sst.aws.Cron("FetchTeamsCron", {
-  job: {
-    vpc,
-    handler: "packages/api/src/lib/getTBAData.fetchTeamsHandler",
-    link: [rds, tbaKey],
-    timeout: "60 seconds",
-    copyFiles: [
-      {
-        from: "node_modules/.prisma/client",
-        to: "packages/.prisma/client",
-      },
-    ],
+    schedule: "rate(10 days)",
   },
-  schedule: "rate(10 days)",
-});
+  {
+    dependsOn: [migrationFunction],
+  }
+);
 
-const fetchMatchesCron = new sst.aws.Cron("FetchMatchesCron", {
-  job: {
-    vpc,
-    handler: "packages/api/src/lib/getTBAData.fetchMatchesHandler",
-    link: [rds, tbaKey],
-    timeout: "60 seconds",
-    copyFiles: [
-      {
-        from: "node_modules/.prisma/client",
-        to: "packages/.prisma/client",
-      },
-    ],
+const fetchTeamsCron = new sst.aws.Cron(
+  "FetchTeamsCron",
+  {
+    job: {
+      vpc,
+      handler: "packages/api/src/lib/getTBAData.fetchTeamsHandler",
+      link: [rds, tbaKey],
+      timeout: "60 seconds",
+      copyFiles: [
+        {
+          from: "node_modules/.prisma/client",
+          to: "packages/.prisma/client",
+        },
+      ],
+    },
+    schedule: "rate(10 days)",
   },
-  schedule: "rate(1 hour)",
-});
+  {
+    dependsOn: [migrationFunction],
+  }
+);
+
+const fetchMatchesCron = new sst.aws.Cron(
+  "FetchMatchesCron",
+  {
+    job: {
+      vpc,
+      handler: "packages/api/src/lib/getTBAData.fetchMatchesHandler",
+      link: [rds, tbaKey],
+      timeout: "60 seconds",
+      copyFiles: [
+        {
+          from: "node_modules/.prisma/client",
+          to: "packages/.prisma/client",
+        },
+      ],
+    },
+    schedule: "rate(1 hour)",
+  },
+  {
+    dependsOn: [migrationFunction],
+  }
+);
 
 if (!$dev) {
-  new aws.lambda.Invocation("FetchTournamentsInvocation", {
-    functionName: fetchTournamentsCron.nodes.job.name,
-    input: JSON.stringify({
-      now: new Date().toISOString(),
-      skipIfFilled: true,
-    }),
-  });
+  new aws.lambda.Invocation(
+    "FetchTournamentsInvocation",
+    {
+      functionName: fetchTournamentsCron.nodes.job.name,
+      input: JSON.stringify({
+        now: new Date().toISOString(),
+        skipIfFilled: true,
+      }),
+    },
+    {
+      dependsOn: [migrationFunction],
+    }
+  );
 
-  new aws.lambda.Invocation("FetchTeamsInvocation", {
-    functionName: fetchTeamsCron.nodes.job.name,
-    input: JSON.stringify({
-      now: new Date().toISOString(),
-      skipIfFilled: true,
-    }),
-  });
+  new aws.lambda.Invocation(
+    "FetchTeamsInvocation",
+    {
+      functionName: fetchTeamsCron.nodes.job.name,
+      input: JSON.stringify({
+        now: new Date().toISOString(),
+        skipIfFilled: true,
+      }),
+    },
+    {
+      dependsOn: [migrationFunction],
+    }
+  );
 
-  new aws.lambda.Invocation("FetchMatchesInvocation", {
-    functionName: fetchMatchesCron.nodes.job.name,
-    input: JSON.stringify({
-      now: new Date().toISOString(),
-      skipIfFilled: true,
-    }),
-  });
+  new aws.lambda.Invocation(
+    "FetchMatchesInvocation",
+    {
+      functionName: fetchMatchesCron.nodes.job.name,
+      input: JSON.stringify({
+        now: new Date().toISOString(),
+        skipIfFilled: true,
+      }),
+    },
+    {
+      dependsOn: [migrationFunction],
+    }
+  );
 }
+
+// MARK: Dev commands
+new sst.x.DevCommand(
+  "PrismaStudio",
+  {
+    dev: {
+      command: "cd packages/api && npx prisma studio --browser none",
+    },
+    environment: {
+      DATABASE_URL: $interpolate`postgresql://${rds.username}:${rds.password}@${rds.host}:${rds.port}/${rds.database}`,
+    },
+  },
+  {
+    dependsOn: [migrationFunction],
+  }
+);
