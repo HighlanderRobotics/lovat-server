2b3d68d57bb283e36959c270ebd4ccb6907b8673
Sid T <thomas.siddharth@gmail.com>
2025-02-02 14:58:26 -0800

    Whoops

--- src/handler/analysis/coreAnalysis/singleMatchSingleScoutReport.ts
+++ src/handler/analysis/coreAnalysis/singleMatchSingleScoutReport.ts
@@ -6,48 +6,6 @@ import { BargeResultPointMap } from '../../manager/managerConstants';
 
 export const singleMatchSingleScoutReport = async (user: User, isPointAverage: boolean, scoutReportUuid: string, metric1: Metric, timeMin = 0, timeMax: number = matchTimeEnd): Promise<number> => {
     try {
-        let position = null
-        let action = null
-        switch (metric1) {
-            case Metric.coralL1:
-                action = EventAction.SCORE_CORAL
-                position = Position.LEVEL_ONE
-                break;
-            case Metric.coralL2:
-                action = EventAction.SCORE_CORAL
-                position = Position.LEVEL_TWO
-                break;
-            case Metric.coralL3:
-                action = EventAction.SCORE_CORAL
-                position = Position.LEVEL_THREE
-                break;
-            case Metric.coralL4:
-                action = EventAction.SCORE_CORAL
-                position = Position.LEVEL_FOUR
-                break;
-            case Metric.netScores:
-                action = EventAction.SCORE_NET
-                break;
-            case Metric.netFails:
-                action = EventAction.FAIL_NET
-                break;
-            case Metric.algaeDrops:
-                action = EventAction.DROP_ALGAE
-                break;
-            case Metric.coralDrops:
-                action = EventAction.DROP_CORAL
-                break;
-            case Metric.processorScores:
-                action = EventAction.SCORE_PROCESSOR
-                break;
-            case Metric.feeds:
-                action = EventAction.FEED
-                position = Position.NONE
-            default:
-                position = Position.NONE
-                break;
-        }
-
         if (metric1 === Metric.driverAbility) {
 
             const match = await prismaClient.scoutReport.findUnique({
