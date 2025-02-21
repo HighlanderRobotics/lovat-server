d5b92de71d9978fe416494ccf6f4c29a2b601be0
CassieColby <70112547+CassieColby@users.noreply.github.com>
2025-02-21 10:06:06 -0800

    knocksAlgae breakdown, quick fix

--- src/handler/analysis/coreAnalysis/nonEventMetric.ts
+++ src/handler/analysis/coreAnalysis/nonEventMetric.ts
@@ -6,42 +6,84 @@ import { MetricsBreakdown } from "../analysisConstants";
 export const nonEventMetric = async (user: User, team: number, metric: MetricsBreakdown): Promise<object> => {
     try {
         // Group reports by metric and count responses
-        const countArray = await prismaClient.scoutReport.groupBy({
-            _count:
-            {
-                scouterUuid: true
-            },
-            where:
-            {
-                teamMatchData:
+        if (metric == MetricsBreakdown.KnocksAlgae) {
+            const countArray = await prismaClient.scoutReport.groupBy({
+                _count:
                 {
-                    teamNumber: team,
-                    tournamentKey:
+                    scouterUuid: true
+                },
+                where:
+                {
+                    teamMatchData:
                     {
-                        in: user.tournamentSource
+                        teamNumber: team,
+                        tournamentKey:
+                        {
+                            in: user.tournamentSource
+                        }
+                    },
+                    scouter:
+                    {
+                        sourceTeamNumber:
+                        {
+                            in: user.teamSource
+                        }
                     }
                 },
-                scouter:
+                by: ['knocksAlgae']
+            })
+
+            // Change into map of metric value -> percentage of reports
+            const totalCount = countArray.reduce((acc, group) => acc + group._count.scouterUuid, 0);
+            const transformedData = countArray.reduce((acc, group) => {
+                const columnValue = group[metric.toString()].toLowerCase();
+                const percentage = group._count.scouterUuid / totalCount;
+                acc[columnValue] = percentage;
+                return acc;
+            }, {});
+
+            return transformedData;
+        }
+        {
+
+
+            const countArray = await prismaClient.scoutReport.groupBy({
+                _count:
                 {
-                    sourceTeamNumber:
+                    scouterUuid: true
+                },
+                where:
+                {
+                    teamMatchData:
                     {
-                        in: user.teamSource
+                        teamNumber: team,
+                        tournamentKey:
+                        {
+                            in: user.tournamentSource
+                        }
+                    },
+                    scouter:
+                    {
+                        sourceTeamNumber:
+                        {
+                            in: user.teamSource
+                        }
                     }
-                }
-            },
-            by: [metric.toString() as keyof ScoutReport]
-        })
+                },
+                by: [metric.toString() as keyof ScoutReport]
+            })
 
-        // Change into map of metric value -> percentage of reports
-        const totalCount = countArray.reduce((acc, group) => acc + group._count.scouterUuid, 0);
-        const transformedData = countArray.reduce((acc, group) => {
-            const columnValue = group[metric.toString()].toLowerCase();
-            const percentage = group._count.scouterUuid / totalCount;
-            acc[columnValue] = percentage;
-            return acc;
-        }, {});
+            // Change into map of metric value -> percentage of reports
+            const totalCount = countArray.reduce((acc, group) => acc + group._count.scouterUuid, 0);
+            const transformedData = countArray.reduce((acc, group) => {
+                const columnValue = group[metric.toString()].toLowerCase();
+                const percentage = group._count.scouterUuid / totalCount;
+                acc[columnValue] = percentage;
+                return acc;
+            }, {});
 
-        return transformedData;
+            return transformedData;
+        }
     }
     catch (error) {
         console.error(error)
