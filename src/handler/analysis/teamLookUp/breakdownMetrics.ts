d084490d02a2cf31c88c0900a58d9e334b5458ec
CassieColby <70112547+CassieColby@users.noreply.github.com>
2025-02-21 12:05:13 -0800

    Undershallowcage naming expection made for breakdowns

--- src/handler/analysis/teamLookUp/breakdownMetrics.ts
+++ src/handler/analysis/teamLookUp/breakdownMetrics.ts
@@ -18,8 +18,16 @@ export const breakdownMetrics = async (req: AuthenticatedRequest, res : Response
         };
         const result = {}
         for (const element in MetricsBreakdown) {
-            result[element] = await nonEventMetric(req.user, params.data.team, MetricsBreakdown[element as keyof typeof MetricsBreakdown])
+            if(element === MetricsBreakdown.underShallowCage)
+            {
+                result["Undershallowcage"] = await nonEventMetric(req.user, params.data.team, MetricsBreakdown[element as keyof typeof MetricsBreakdown])
+            }
+            else
+            {
+                result[element.toLowerCase()] = await nonEventMetric(req.user, params.data.team, MetricsBreakdown[element as keyof typeof MetricsBreakdown])
+            }
         };
+        console.log(result)
         res.status(200).send(result)
     }
     catch (error) {
