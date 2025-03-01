8ca25b7eae341cb2cde31bfcfb13013644ed9007
Sid T <thomas.siddharth@gmail.com>
2025-03-01 14:39:43 -0800

    remap picklist viewing

--- src/handler/manager/getSinglePicklist.ts
+++ src/handler/manager/getSinglePicklist.ts
@@ -20,14 +20,37 @@ export const getSinglePicklist = async (req: AuthenticatedRequest, res: Response
 
         const row = await prismaClient.sharedPicklist.findUnique({
             where: {
-                uuid: params.data.uuid,
                 author: {
                     teamNumber: user.teamNumber
-                }
+                },
+                uuid: params.data.uuid
             }
 
         })
-        res.status(200).send(row);
+
+        // Map picklist rows to accepted names
+        const out = {
+            uuid: row.uuid,
+            name: row.name,
+            authorId: row.authorId,
+            totalpoints: row.totalPoints,
+            autopoints: row.autoPoints,
+            teleoppoints: row.teleopPoints,
+            driverability: row.driverAbility,
+            bargeresult: row.barge,
+            level1: row.coralLevel1Scores,
+            level2: row.coralLevel2Scores,
+            level3: row.coralLevel3Scores,
+            level4: row.coralLevel4Scores,
+            coralpickup: row.coralPickups,
+            algaeProcessor: row.algaeProcessor,
+            algaeNet: row.algaeNet,
+            algaePickups: row.algaePickups,
+            feeds: row.feeds,
+            defends: row.defense,
+        }
+
+        res.status(200).send(out);
     }
     catch (error) {
         console.error(error)
