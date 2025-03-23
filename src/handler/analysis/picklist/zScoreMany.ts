aae1ea7fdba9d9c2d6bfd780dc36d034e6b59abc
Sid T <thomas.siddharth@gmail.com>
2025-03-23 15:14:10 -0700

    Add try catch wrapper to zScoreMany

--- src/handler/analysis/picklist/zScoreMany.ts
+++ src/handler/analysis/picklist/zScoreMany.ts
@@ -2,78 +2,86 @@ import { Metric, metricsCategory, metricToName, picklistToMetric } from "../anal
 import ss from "simple-statistics";
 import { rankFlag } from "../rankFlag";
 
-export const zScoreMany = async (data: Partial<Record<Metric, { average: number }[]>>, teams: number[], eventKey: string, queries: Record<string, number>, flags: string[]): Promise<typeof results> => {
-    const results: {
-        team: number,
-        result: number,
-        breakdown: { type: string, result: number }[],
-        unweighted: { type: string, result: number }[],
-        flags: { type: string, result: number }[]
-    }[] = [];
+export const zScoreMany = async (data: Partial<Record<Metric, { average: number }[]>>, teams: number[], eventKey: string, queries: Record<string, number>, flags: string[]) => {
+    try {
+        const results: {
+            team: number,
+            result: number,
+            breakdown: { type: string, result: number }[],
+            unweighted: { type: string, result: number }[],
+            flags: { type: string, result: number }[]
+        }[] = [];
 
-    // Include flagged metrics
-    const includedMetrics = new Array<Metric>;
-    for (const metric of metricsCategory) {
-        if (flags.includes(metricToName[metric])) {
-            includedMetrics.push(metric);
+        console.log(flags);
+
+        // Include flagged metrics
+        const includedMetrics = new Array<Metric>;
+        for (const metric of metricsCategory) {
+            if (flags.includes(metricToName[metric])) {
+                includedMetrics.push(metric);
+            }
         }
-    }
 
-    // Flags (held as category metrics) and object initialization first
-    teams.forEach((team, i) => {
-        results[i] ||= {
-            team: team,
-            result: 0,
-            breakdown: [],
-            unweighted: [],
-            flags: []
-        };
+        // Flags (held as category metrics) and object initialization first
+        teams.forEach((team, i) => {
+            results[i] ||= {
+                team: team,
+                result: 0,
+                breakdown: [],
+                unweighted: [],
+                flags: []
+            };
 
-        for (const metric in includedMetrics) {
-            results[i].flags.push({ type: metricToName[metric], result: data[metric][team].average });
+            for (const metric in includedMetrics) {
+                results[i].flags.push({ type: metricToName[metric], result: data[metric][team].average });
+            }
+        });
+
+        // Picklist rankings and zScore
+        for (const picklistParam of Object.keys(picklistToMetric)) {
+            if (queries[picklistParam]) {
+                // Initialize variables for parameter
+                const currAvgs = data[picklistToMetric[picklistParam]];
+                const condensedAvgs: number[] = [];
+                currAvgs.forEach(e => condensedAvgs.push(e.average));
+
+                const avg = condensedAvgs.reduce((acc, cur) => acc + cur, 0) / teams.length;
+                const std = ss.standardDeviation(condensedAvgs);
+
+                teams.forEach((team, i) => {
+                    let zScore = 0
+
+                    // If there is a meaningful datapoint, calculate zScore
+                    if (currAvgs[team].average !== 0) {
+                        // Default standard deviation to 0.1
+                        zScore = (currAvgs[team].average - avg) / (std || 0.1);
+                    }
+
+                    // Push scores
+                    results[i].breakdown.push({ type: picklistParam, result: zScore * queries[picklistParam] });
+                    results[i].unweighted.push({ type: picklistParam, result: zScore });
+                });
+            }
         }
-    });
 
-    // Picklist rankings and zScore
-    for (const picklistParam of Object.keys(picklistToMetric)) {
-        if (queries[picklistParam]) {
-            // Initialize variables for parameter
-            const currAvgs = data[picklistToMetric[picklistParam]];
-            const condensedAvgs: number[] = [];
-            currAvgs.forEach(e => condensedAvgs.push(e.average));
-
-            const avg = condensedAvgs.reduce((acc, cur) => acc + cur, 0) / teams.length;
-            const std = ss.standardDeviation(condensedAvgs);
+        // Deal with rankings
+        if (flags.includes("rank")) {
+            const rankings = await rankFlag(eventKey, ...teams);
 
             teams.forEach((team, i) => {
-                let zScore = 0
-
-                // If there is a meaningful datapoint, calculate zScore
-                if (currAvgs[team].average !== 0) {
-                    // Default standard deviation to 0.1
-                    zScore = (currAvgs[team].average - avg) / (std || 0.1);
-                }
-
-                // Push scores
-                results[i].breakdown.push({ type: picklistParam, result: zScore * queries[picklistParam] });
-                results[i].unweighted.push({ type: picklistParam, result: zScore });
+                results[i].flags.push({ type: "rank", result: rankings[team] });
             });
         }
+
+        // Sum total z score
+        for (let i = 0; i < results.length; i++) {
+            results[i].result = results[i].breakdown.reduce((acc, cur) => acc + cur.result, 0);
+        }
+
+        return results;
     }
-
-    // Deal with rankings
-    if (flags.includes("rank")) {
-        const rankings = await rankFlag(eventKey, ...teams);
-
-        teams.forEach((team, i) => {
-            results[i].flags.push({ type: "rank", result: rankings[team] });
-        });
+    catch (error) {
+        console.log(error)
+        throw (error)
     }
-
-    // Sum total z score
-    for (let i = 0; i < results.length; i++) {
-        results[i].result = results[i].breakdown.reduce((acc, cur) => acc + cur.result, 0);
-    }
-
-    return results;
 }
