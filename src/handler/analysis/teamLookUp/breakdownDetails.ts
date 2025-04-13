09e1e834e45e99ed792eda1b9ce66f6fb08c68d7
Sid T <thomas.siddharth@gmail.com>
2025-04-13 14:49:07 -0700

    add breakdown timeline

---
+++ src/handler/analysis/teamLookUp/breakdownDetails.ts
@@ -0,0 +1,76 @@
+import { Response } from "express";
+import z from 'zod'
+import prismaClient from '../../../prismaClient'
+import { AuthenticatedRequest } from "../../../lib/middleware/requireAuth";
+import { lowercaseToBreakdown, MetricsBreakdown } from "../analysisConstants";
+
+
+export const breakdownDetails = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const params = z.object({
+            team: z.number(),
+            breakdown: z.nativeEnum(MetricsBreakdown)
+        }).safeParse({
+            team: Number(req.params.team),
+            breakdown: lowercaseToBreakdown[req.params.breakdown]
+        })
+        if (!params.success) {
+            console.log(params)
+            res.status(400).send(params);
+            return;
+        };
+
+        const query = `
+        SELECT "${params.data.breakdown}" AS breakdown,
+            "teamMatchKey" AS key,
+            tmnt."name" AS tournament,
+            sc."sourceTeamNumber" AS sourceteam,
+            teamScouter."name" AS scouter
+        FROM "ScoutReport" s
+        JOIN "Scouter" sc ON sc."uuid" = s."scouterUuid"
+        JOIN "TeamMatchData" tmd
+            ON tmd."teamNumber" = ${params.data.team}
+            AND tmd."key" = s."teamMatchKey"
+            AND sc."sourceTeamNumber" = ANY($1)
+            AND tmd."tournamentKey" = ANY($2)
+        JOIN "Tournament" tmnt ON tmd."tournamentKey" = tmnt."key"
+        LEFT JOIN "Scouter" teamScouter
+            ON teamScouter."uuid" = s."scouterUuid"
+            AND teamScouter."sourceTeamNumber" = ${req.user.teamNumber}
+        ORDER BY tmnt."date" DESC, tmd."matchType" DESC, tmd."matchNumber" DESC
+        `;
+
+        interface QueryRow {
+            breakdown: string;
+            key: string;
+            tournament: string;
+            sourceteam: string;
+            scouter: string;
+        }
+
+        const data = await prismaClient.$queryRawUnsafe<QueryRow[]>(
+            query,
+            req.user.teamSource,
+            req.user.tournamentSource
+        );
+
+        console.log(data.slice(0, 3))
+
+        const result: { key: string, tournamentName: string, breakdown: string, sourceTeam: string, scouter?: string }[] = [];
+        for (const match of data) {
+            result.push({
+                key: match.key,
+                tournamentName: match.tournament,
+                breakdown: match.breakdown,
+                sourceTeam: match.sourceteam,
+                scouter: match.scouter ?? undefined
+            });
+        }
+
+        res.status(200).send(result);
+    }
+    catch (error) {
+        console.error(error)
+        res.status(400).send(error)
+    }
+};
