920bc3d9e87087f498ad943d341a6be76500ecda
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2025-09-25 17:27:30 -0700

    Revert "verify events"
    This reverts commit 9f8988744a63a7bff9eb5acc9c62fb0e40f8651a.

--- src/handler/slack/processEvents.ts
+++ src/handler/slack/processEvents.ts
@@ -1,5 +1,38 @@
 import { Request, Response } from "express";
+import prismaClient from '../../prismaClient'
+import z from 'zod'
 
 export const processEvent = async (req: Request, res: Response): Promise<void> => {
-    res.status(200).send(req.body.challenge)
+    const params = z.object({
+        type: z.literal("event_callback"),
+        token: z.string(),
+        team_id: z.string(),
+        api_app_id: z.string(),
+        event: z.union([
+            z.object(
+                {
+                type: z.literal("channel_deleted"),
+                channel: z.string()
+                }
+            ), z.object(
+                {
+                type: z.literal("app_uninstalled"),
+                }
+            )]),
+            event_id: z.string(),
+        }).parse(req.body);
+
+    if (params.event.type === "app_uninstalled") {
+        await prismaClient.slackWorkspace.deleteMany({
+            where: {
+                workspaceId: params.team_id
+            }
+        })
+    } else if (params.event.type === "channel_deleted") {
+        await prismaClient.slackSubscription.deleteMany({
+            where: {
+                channelId: params.event.channel
+            }
+        })
+    }
 }
