a17ea439a73ee9806908e6470c8d7ca42ee4d235
MangoSwirl <collin.camerono@gmail.com>
2025-10-12 11:41:20 -0700

    Prevent fetch on startup in development

--- src/lib/getTBAData.ts
+++ src/lib/getTBAData.ts
@@ -1,18 +1,29 @@
 import fetchTournaments from "./fetchTournaments";
 import fetchTeams from "./fetchTeams";
 import fetchMatches from "./fetchMatches";
+import prisma from "../prismaClient";
 
 export default async function getTBAData(): Promise<void> {
   const year = 2024;
-  // Import tournaments
-  console.log("Fetching tournaments from TBA...");
-  await fetchTournaments(year);
-  console.log("Done fetching tournaments from TBA.");
+  const devEnabled = process.env.NODE_ENV === "development";
 
-  // Import teams
-  console.log("Fetching teams from TBA...");
-  await fetchTeams();
-  console.log("Done fetching teams from TBA.");
+  // Prevent fetch on auto-restart in dev mode
+  const skipInitialFetch =
+    devEnabled &&
+    (await prisma.tournament.count()) > 0 &&
+    (await prisma.team.count()) > 0;
+
+  if (!skipInitialFetch) {
+    // Import tournaments
+    console.log("Fetching tournaments from TBA...");
+    await fetchTournaments(year);
+    console.log("Done fetching tournaments from TBA.");
+
+    // Import teams
+    console.log("Fetching teams from TBA...");
+    await fetchTeams();
+    console.log("Done fetching teams from TBA.");
+  }
 
   // repeat every 24 hours
   setInterval(async () => {
