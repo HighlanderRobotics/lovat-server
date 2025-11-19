b6d658e11cf2b0f26e3b662388d0c89fda16b62e
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2025-11-18 19:29:01 -0800

    add a clearCache method

---
+++ src/lib/clearCache.ts
@@ -0,0 +1,10 @@
+import prismaClient from "../prismaClient";
+import { kv } from "../redisClient";
+
+const clearCache = async () => {
+    await prismaClient.cachedAnalysis.deleteMany()
+
+    kv.flush()
+
+    console.log("Cache cleared")
+}
