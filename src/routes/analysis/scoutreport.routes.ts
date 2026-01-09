0ade056822c6d79878777f537b6ccd482f24f54e
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2026-01-08 16:45:25 -0800

    hotfix: add .js to imports

--- src/routes/analysis/scoutreport.routes.ts
+++ src/routes/analysis/scoutreport.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { requireAuth } from "../../lib/middleware/requireAuth.js";
-import { matchPageSpecificScouter } from "../../handler/analysis/specificMatchPage/matchPageSpecificScouter";
-import { scoutReportForMatch } from "../../handler/analysis/specificMatchPage/scoutReportForMatch";
-import { timelineForScoutReport } from "../../handler/analysis/specificMatchPage/timelineForScoutReport";
+import { matchPageSpecificScouter } from "../../handler/analysis/specificMatchPage/matchPageSpecificScouter.js";
+import { scoutReportForMatch } from "../../handler/analysis/specificMatchPage/scoutReportForMatch.js";
+import { timelineForScoutReport } from "../../handler/analysis/specificMatchPage/timelineForScoutReport.js";
 
 const router = Router();
 
