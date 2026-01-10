40a19f8b5cf87b49900a60247c989b1bf20a84df
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2026-01-10 15:07:00 -0800

    Revert "move addNotOnTeam.ts"
    This reverts commit 8f5b2845a95229f4884d54b871a176e0cfb2e517.

--- src/handler/manager/temp/addNotOnTeam.ts
+++ src/handler/manager/temp/addNotOnTeam.ts
@@ -1,10 +1,10 @@
 import { Response } from "express";
-import prismaClient from "../../prismaClient.js";
-import { AuthenticatedRequest } from "../../lib/middleware/requireAuth.js";
+import prismaClient from "../../../prismaClient.js";
+import { AuthenticatedRequest } from "../../../lib/middleware/requireAuth.js";
 
 export const addNotOnTeam = async (
   req: AuthenticatedRequest,
-  res: Response
+  res: Response,
 ): Promise<void> => {
   try {
     const user = await prismaClient.user.update({
