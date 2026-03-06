6d2ce0012cd8a01cab473287aa8f2420aa3487dd
JackAttack-365 <142643773+jackattack-4@users.noreply.github.com>
2026-03-05 16:56:41 -0800

    move docs to /v1/doc

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,6 @@ import { posthog } from "./posthogClient.js";
 import posthogReporter from "./lib/middleware/posthogMiddleware.js";
 
 import routes from "./routes/index.js";
-import swaggerUi from "swagger-ui-express";
-import { generateOpenApiDocument } from "./lib/openapi.js";
 import path from "path";
 
 export const app = express();
@@ -25,7 +23,7 @@ app.use(
       process.env.NODE_ENV === "development"
         ? true // any origin during development (eg. any port on localhost)
         : /^https:\/\/(.*\.)?lovat\.app$/, // only from lovat.app
-  })
+  }),
 );
 
 app.use(bodyParser.json());
@@ -42,22 +40,3 @@ app.use("/v1", routes); //theo was here
 app.get("/status", (req, res) => {
   res.status(200).send("Server running");
 });
-
-// OpenAPI docs
-const isDevelopment = process.env.NODE_ENV === "development";
-const openApiDocument = !isDevelopment ? generateOpenApiDocument() : undefined;
-
-app.get("/doc.json", (_req, res) => {
-  const doc = isDevelopment ? generateOpenApiDocument() : openApiDocument;
-  res.json(doc);
-});
-
-app.use(
-  "/doc",
-  swaggerUi.serve,
-  swaggerUi.setup(isDevelopment ? undefined : openApiDocument, {
-    // In development, Swagger UI will fetch the spec from /doc.json on each request.
-    swaggerOptions: isDevelopment ? { url: "/doc.json" } : undefined,
-    customCssUrl: "/swaggerTheme.css",
-  }),
-);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import slackRouter from "./slack/slack.routes.js";
 import managerRouter from "./manager/manager.routes.js";
 import analysisRouter from "./analysis/analysis.routes.js";
 import { onboardingRedirect } from "../handler/slack/onboardingRedirect.js";
+import { generateOpenApiDocument } from "../lib/openapi.js";
+import swaggerUi from "swagger-ui-express";
 
 const router = Router();
 
@@ -13,4 +15,22 @@ router.use("/analysis", analysisRouter);
 
 router.get("/slack-invite", onboardingRedirect);
 
+// OpenAPI docs
+const isDevelopment = process.env.NODE_ENV === "development";
+const openApiDocument = !isDevelopment ? generateOpenApiDocument() : undefined;
+
+router.get("/doc.json", (_req, res) => {
+  const doc = isDevelopment ? generateOpenApiDocument() : openApiDocument;
+  res.json(doc);
+});
+
+router.use(
+  "/doc",
+  swaggerUi.serve,
+  swaggerUi.setup(isDevelopment ? undefined : openApiDocument, {
+    // In development, Swagger UI will fetch the spec from /doc.json on each request.
+    swaggerOptions: isDevelopment ? { url: "/v1/doc.json" } : undefined,
+    customCssUrl: "/swaggerTheme.css",
+  }),
+);
 export default router;
